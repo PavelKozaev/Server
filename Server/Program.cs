07a08b8630ffe6558ad43bea02414c17bc29c518
Pavel Kozaev <pkozaev221299@gmail.com>
2023-09-02 18:56:55 +0300

    Добавить ответ сервера

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,14 +11,18 @@ namespace Server
             listener.Start();
 
             var client = listener.AcceptTcpClient();
+
             using (var stream = client.GetStream())
             {
                 using (var reader = new StreamReader(stream))
+                using (var writer = new StreamWriter(stream))
                 {
-                    while (true)
+                    for (string line = null; line != string.Empty; line = reader.ReadLine())
                     {
-                        Console.WriteLine(reader.ReadLine());
+                        Console.WriteLine(line);
                     }
+
+                    writer.WriteLine("Hello from server!");
                 }
             }
         }
