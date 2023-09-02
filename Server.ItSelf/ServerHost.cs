60f52f4034f7e4cb761e4cbc4379846897b6dcc4
Pavel Kozaev <pkozaev221299@gmail.com>
2023-09-02 19:12:09 +0300

    Организовать классы в проекте

---
+++ Server.ItSelf/ServerHost.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace Server.ItSelf
+{
+    public class ServerHost
+    {
+        public ServerHost()
+        {
+            
+        }
+        public void Start()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 80);
+            listener.Start();
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+
+                using (var stream = client.GetStream())
+                {
+                    using (var reader = new StreamReader(stream))
+                    using (var writer = new StreamWriter(stream))
+                    {
+                        for (string line = null; line != string.Empty; line = reader.ReadLine())
+                        {
+                            Console.WriteLine(line);
+                        }
+
+                        writer.WriteLine("Hello from server!");
+                    }
+                }
+            }
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using System.Net.Sockets;
+﻿using Server.ItSelf;
 
 namespace Server
 {
@@ -7,24 +6,8 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            TcpListener listener = new TcpListener(IPAddress.Any, 80);
-            listener.Start();
-
-            var client = listener.AcceptTcpClient();
-
-            using (var stream = client.GetStream())
-            {
-                using (var reader = new StreamReader(stream))
-                using (var writer = new StreamWriter(stream))
-                {
-                    for (string line = null; line != string.Empty; line = reader.ReadLine())
-                    {
-                        Console.WriteLine(line);
-                    }
-
-                    writer.WriteLine("Hello from server!");
-                }
-            }
+            ServerHost host = new ServerHost();
+            host.Start();
         }
     }
 }
