f129018fe81118db6c59a8e2c01a48cdeeefa576
Pavel Kozaev <pkozaev221299@gmail.com>
2023-09-02 18:21:37 +0300

    Ввывести в консоль данные от HTTP-клиента

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 80);
+            listener.Start();
+
+            var client = listener.AcceptTcpClient();
+            using (var stream = client.GetStream())
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    while (true)
+                    {
+                        Console.WriteLine(reader.ReadLine());
+                    }
+                }
+            }
+        }
+    }
+}
